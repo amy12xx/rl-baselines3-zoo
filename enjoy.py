1ac9f6142611860969aa6fdfbbd2550a8d1c3d03
Amanda Dsouza <meezamanda@yahoo.com>
2021-11-13 23:11:16 +0530

    resize from command line

--- enjoy.py
+++ enjoy.py
@@ -67,7 +67,7 @@ def main():  # noqa: C901
     parser.add_argument("--reward-threshold", help="Reward threshold", type=int, default=0)
     parser.add_argument("--img-obs", help="Save observations as images", action="store_true", default=False)
     parser.add_argument("--frame-stack", help="Frame stacking", type=int, default=4)
-    parser.add_argument("--render-dim", help="Image dimensions", type=tuple, default=(250, 250))
+    parser.add_argument("--render-dim", help="Image dimensions", type=int, default=None)
     args = parser.parse_args()
 
     # Going through custom gym packages to let them register in the global registory
@@ -193,7 +193,8 @@ def main():  # noqa: C901
     # to save observations from Fetch env
     if args.img_obs:
         img_obs = env.render("rgb_array")
-        img_obs = cv2.resize(img_obs, (args.render_dim[0], args.render_dim[1]), interpolation=cv2.INTER_CUBIC)
+        if args.render_dim is not None:
+            img_obs = cv2.resize(img_obs, (args.render_dim, args.render_dim), interpolation=cv2.INTER_CUBIC)
         ep_obs.append(img_obs)
     else:
         ep_obs.append(obs["observation"])
@@ -238,7 +239,8 @@ def main():  # noqa: C901
                     obs = env.reset()
                 if args.img_obs:
                     img_obs = env.render("rgb_array")
-                    img_obs = cv2.resize(img_obs, (args.render_dim[0], args.render_dim[1]), interpolation=cv2.INTER_CUBIC)
+                    if args.render_dim is not None:
+                        img_obs = cv2.resize(img_obs, (args.render_dim, args.render_dim), interpolation=cv2.INTER_CUBIC)
                     ep_obs.append(img_obs)
                 else:
                     ep_obs.append(obs["observation"])
