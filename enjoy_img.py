064d303c1212af85bce402eed38db08014af44d2
Amanda Dsouza <meezamanda@yahoo.com>
2022-05-12 09:17:12 -0700

    original enjoy

--- enjoy_img.py
+++ enjoy_img.py
@@ -3,17 +3,52 @@ import glob
 import importlib
 import os
 import sys
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch as th
 import yaml
+import gym
+import cv2
+from gym import spaces
 from stable_baselines3.common.utils import set_random_seed
-
+# from stable_baselines3.common.vec_env import VecFrameStack
 import utils.import_envs  # noqa: F401 pylint: disable=unused-import
 from utils import ALGOS, create_test_env, get_latest_run_id, get_saved_hyperparams
 from utils.exp_manager import ExperimentManager
 from utils.utils import StoreDict
 
+
+def compute_stacking(
+        num_envs: int,
+        n_stack: int,
+        render_dim: int,
+        ) -> Tuple[bool, int, np.ndarray, int]:
+        channels_first = False
+
+        # This includes the vec-env dimension (first)
+        stack_dimension = 1 if channels_first else -1
+        repeat_axis = 0 if channels_first else -1
+        low = np.repeat(np.zeros((render_dim, render_dim, 3)), n_stack, axis=repeat_axis)
+        stackedobs = np.zeros((num_envs,) + low.shape, low.dtype)
+        return channels_first, stack_dimension, stackedobs, repeat_axis
+
+
+def update_stacked_obs(observations, stackedobs, stack_dimension=-1):
+    stack_ax_size = observations.shape[stack_dimension]
+    stackedobs = np.roll(stackedobs, shift=-stack_ax_size, axis=stack_dimension)
+    stackedobs[..., -observations.shape[stack_dimension]:] = observations
+
+    return stackedobs
+
+
+def get_img(env, render_dim):
+    try:
+        img_obs = env.render("rgb_array", width=render_dim, height=render_dim)
+    except:
+        img_obs = env.render("rgb_array")
+        img_obs = cv2.resize(img_obs, (render_dim, render_dim), interpolation=cv2.INTER_CUBIC)
+    return img_obs
 
 def main():  # noqa: C901
     parser = argparse.ArgumentParser()
@@ -29,7 +64,6 @@ def main():  # noqa: C901
         "--no-render", action="store_true", default=False, help="Do not render the environment (useful for tests)"
     )
     parser.add_argument("--deterministic", action="store_true", default=False, help="Use deterministic actions")
-    parser.add_argument("--device", help="PyTorch device to be use (ex: cpu, cuda...)", default="auto", type=str)
     parser.add_argument(
         "--load-best", action="store_true", default=False, help="Load best model instead of last model if available"
     )
@@ -61,6 +95,12 @@ def main():  # noqa: C901
     parser.add_argument(
         "--env-kwargs", type=str, nargs="+", action=StoreDict, help="Optional keyword argument to pass to the env constructor"
     )
+    parser.add_argument("--save-episodes", action="store_true", default=False, help="Save episodes")
+    parser.add_argument("--reward-threshold", help="Reward threshold", type=int, default=0)
+    parser.add_argument("--img-obs", help="Save observations as images", action="store_true", default=False)
+    parser.add_argument("--frame-stack", help="Frame stacking", type=int, default=4)
+    parser.add_argument("--render-dim", help="Image dimensions", type=int, default=None)
+    parser.add_argument("--dense-reward", help="If dense reward, done flag will not be checked while saving trajectories", action="store_true", default=False)
     args = parser.parse_args()
 
     # Going through custom gym packages to let them register in the global registory
@@ -130,6 +170,8 @@ def main():  # noqa: C901
         th.set_num_threads(args.num_threads)
 
     is_atari = ExperimentManager.is_atari(env_id)
+    is_robotics = ExperimentManager.is_robotics_env(env_id)
+    print("is_atari={}, is_robotics={}".format(is_atari, is_robotics))
 
     stats_path = os.path.join(log_path, env_id)
     hyperparams, stats_path = get_saved_hyperparams(stats_path, norm_reward=args.norm_reward, test_mode=True)
@@ -138,7 +180,7 @@ def main():  # noqa: C901
     env_kwargs = {}
     args_path = os.path.join(log_path, env_id, "args.yml")
     if os.path.isfile(args_path):
-        with open(args_path) as f:
+        with open(args_path, "r") as f:
             loaded_args = yaml.load(f, Loader=yaml.UnsafeLoader)  # pytype: disable=module-attr
             if loaded_args["env_kwargs"] is not None:
                 env_kwargs = loaded_args["env_kwargs"]
@@ -176,9 +218,38 @@ def main():  # noqa: C901
             "clip_range": lambda _: 0.0,
         }
 
-    model = ALGOS[algo].load(model_path, env=env, custom_objects=custom_objects, device=args.device, **kwargs)
+    model = ALGOS[algo].load(model_path, env=env, custom_objects=custom_objects, **kwargs)
+
+    save_episode_obs, save_episode_acts = [], []
+    save_episode_stackedobs = []
+    ep_obs, ep_acts = [], []
+
+    channels_first, stack_dimension, stackedobs, repeat_axis = compute_stacking(1, args.frame_stack, args.render_dim)
+    ep_stacked_obs = []
 
     obs = env.reset()
+
+    # to save observations from Fetch env
+    if args.img_obs:
+        if is_atari:
+            ep_obs.append(obs)
+            stackedobs = update_stacked_obs(obs, stackedobs)
+            ep_stacked_obs.append(stackedobs)
+        elif args.render_dim is not None:
+            img_obs = get_img(env, args.render_dim)
+            img_obs = img_obs.astype(np.uint8)
+            ep_obs.append(img_obs)
+            stackedobs = update_stacked_obs(img_obs, stackedobs)
+            ep_stacked_obs.append(stackedobs)
+        else:
+            img_obs = env.render("rgb_array")
+            ep_obs.append(img_obs)
+            stackedobs = update_stacked_obs(img_obs, stackedobs)
+            ep_stacked_obs.append(stackedobs)
+    else:
+        ep_obs.append(obs["observation"])
+        stackedobs = update_stacked_obs(obs["observation"], stackedobs)
+        ep_stacked_obs.append(stackedobs)
 
     # Deterministic by default except for atari games
     stochastic = args.stochastic or is_atari and not args.deterministic
@@ -190,15 +261,21 @@ def main():  # noqa: C901
     ep_len = 0
     # For HER, monitor success rate
     successes = []
+
+    print("Running for {} timesteps".format(args.n_timesteps))
     try:
         for _ in range(args.n_timesteps):
             action, state = model.predict(obs, state=state, deterministic=deterministic)
             obs, reward, done, infos = env.step(action)
+            ep_acts.append(action)
+
             if not args.no_render:
                 env.render("human")
 
             episode_reward += reward[0]
             ep_len += 1
+
+            print("rew {}, len {}, done {}".format(episode_reward, ep_len, done))
 
             if args.n_envs == 1:
                 # For atari the return reward is not the atari score
@@ -208,6 +285,45 @@ def main():  # noqa: C901
                     if episode_infos is not None:
                         print(f"Atari Episode Score: {episode_infos['r']:.2f}")
                         print("Atari Episode Length", episode_infos["l"])
+                if done:
+                    print("Episode reward: {}".format(episode_reward))
+                    if episode_reward >= args.reward_threshold:
+                        assert len(ep_obs) == len(ep_acts), "len not same: {}, {}".format(len(ep_obs), len(ep_acts))
+                        save_episode_obs.append(ep_obs)
+                        save_episode_acts.append(ep_acts)
+                        save_episode_stackedobs.append(ep_stacked_obs)
+                    ep_obs, ep_acts = [], []
+                    ep_stacked_obs = []
+                    obs = env.reset()
+                elif args.dense_reward:
+                    if episode_reward >= args.reward_threshold:
+                        print("Episode reward: {}".format(episode_reward))
+                        save_episode_obs.append(ep_obs)
+                        save_episode_acts.append(ep_acts)
+                        save_episode_stackedobs.append(ep_stacked_obs)
+                    ep_obs, ep_acts = [], []
+                    ep_stacked_obs = []
+                if args.img_obs:
+                    # img_obs = env.render("rgb_array")
+                    if is_atari:
+                        ep_obs.append(obs)
+                        stackedobs = update_stacked_obs(obs, stackedobs)
+                        ep_stacked_obs.append(stackedobs)
+                    elif args.render_dim is not None:
+                        img_obs = get_img(env, args.render_dim)
+                        img_obs = img_obs.astype(np.uint8)
+                        ep_obs.append(img_obs)
+                        stackedobs = update_stacked_obs(img_obs, stackedobs)
+                        ep_stacked_obs.append(stackedobs)
+                    else:
+                        img_obs = env.render("rgb_array")
+                        ep_obs.append(img_obs)
+                        stackedobs = update_stacked_obs(img_obs, stackedobs)
+                        ep_stacked_obs.append(stackedobs)
+                else:
+                    ep_obs.append(obs["observation"])
+                    stackedobs = update_stacked_obs(obs["observation"], stackedobs)
+                    ep_stacked_obs.append(stackedobs)
 
                 if done and not is_atari and args.verbose > 0:
                     # NOTE: for env using VecNormalize, the mean reward
@@ -229,7 +345,9 @@ def main():  # noqa: C901
                         successes.append(infos[0].get("is_success", False))
                         episode_reward, ep_len = 0.0, 0
 
-    except KeyboardInterrupt:
+    # except KeyboardInterrupt:
+    except Exception as e:
+        print("ERROR: ", e)
         pass
 
     if args.verbose > 0 and len(successes) > 0:
@@ -243,6 +361,28 @@ def main():  # noqa: C901
         print(f"Mean episode length: {np.mean(episode_lengths):.2f} +/- {np.std(episode_lengths):.2f}")
 
     env.close()
+
+    #save episodes
+    print("Saving episodes...")
+    save_episode_obs = [obs for ep in save_episode_obs for obs in ep]
+    save_episode_acts = [act for ep in save_episode_acts for act in ep]
+    obs = np.array(save_episode_obs)
+    acts = np.array(save_episode_acts)
+
+    save_episode_stackedobs = [obs for ep in save_episode_stackedobs for obs in ep]
+    stackedobs = np.array(save_episode_stackedobs)
+    stackedobs = np.squeeze(stackedobs, axis=1)
+    print(stackedobs.shape)
+
+    if args.img_obs:
+        # episodes = obs
+        episodes = stackedobs
+    else:
+        obs = obs.reshape(len(obs), -1)
+        acts = acts.reshape(len(acts), -1)
+        episodes = np.hstack((obs, acts))
+    print("Episode shape: ", episodes.shape)
+    np.save("{}/expert_{}".format(log_path, args.env), episodes)
 
 
 if __name__ == "__main__":
